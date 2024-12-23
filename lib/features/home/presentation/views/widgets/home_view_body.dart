8cb9045f5adc7ab9e1057de672620ac4ef68b586
ismailrabie <ismailrabie111@gmail.com>
2024-12-23 08:38:27 +0200

    add customAppbar to home view

---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'custom_appbar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [CustomAppbar()],
+    );
+  }
+}
