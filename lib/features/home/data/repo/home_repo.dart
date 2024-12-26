f0894cff5ee91e1387da7d0ca3cb245868bb6531
ismailrabie <ismailrabie111@gmail.com>
2024-12-26 11:49:54 +0200

    homeRepo Done

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,6 +1,11 @@
- abstract class HomeRepo{
+ import 'package:bookapp/features/home/data/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+
+abstract class HomeRepo{
   //بحدد شو ححط هان دوال بس الاسم
-   fetchBestSellerBooks();
-   fetchFeaturedBooks();
+   Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+   Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 
  }
