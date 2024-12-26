0df9e5a9066b4d94c10e5dd0b8b80020f2bffd3f
ismailrabie <ismailrabie111@gmail.com>
2024-12-26 07:46:02 +0200

    create custom app bar for bookDetails

--- lib/features/home/presentation/views/widgets/custom_appbar.dart
+++ lib/features/home/presentation/views/widgets/custom_appbar.dart
@@ -9,7 +9,7 @@ class CustomAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 40),
+      padding:  const EdgeInsets.only(top: 40,bottom: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
