76cd3fc74e7d0cde73701fcdcf1447b5171ed5de
ismailrabie <ismailrabie111@gmail.com>
2024-12-26 07:41:22 +0200

    some edit in constants class

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-const kPrimeColor=Color(0xff100B20);
+const kPrimeColor = Color(0xff100B20);
+const kTransactionDuration = Duration(milliseconds: 250);
+const kGtSectraFine = 'GT Sectra Fine';
+const kFeaturedBox = 'featured_box';
+const kNewestBox = 'newest_box';
