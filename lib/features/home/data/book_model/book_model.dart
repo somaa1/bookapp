d55a30a10ecd6d130902819776ff610d060f20f7
ismailrabie <ismailrabie111@gmail.com>
2024-12-27 11:06:18 +0200

    refactor model

--- lib/features/home/data/book_model/book_model.dart
+++ lib/features/home/data/book_model/book_model.dart
@@ -1,51 +1,71 @@
-import 'package:bookapp/features/home/data/book_model/sale_info.dart';
-import 'package:bookapp/features/home/data/book_model/volume_info.dart';
+import 'package:equatable/equatable.dart';
 
 import 'access_info.dart';
+import 'sale_info.dart';
+import 'search_info.dart';
+import 'volume_info.dart';
 
-class BookModel {
-  BookModel({
+class BookModel extends Equatable {
+  final String? kind;
+  final String? id;
+  final String? etag;
+  final String? selfLink;
+  final VolumeInfo volumeInfo;
+  final SaleInfo? saleInfo;
+  final AccessInfo? accessInfo;
+  final SearchInfo? searchInfo;
+
+  const BookModel({
     this.kind,
     this.id,
     this.etag,
     this.selfLink,
-    this.volumeInfo,
+    required this.volumeInfo,
     this.saleInfo,
-    this.accessInfo,});
+    this.accessInfo,
+    this.searchInfo,
+  });
 
-  BookModel.fromJson(dynamic json) {
-    kind = json['kind'];
-    id = json['id'];
-    etag = json['etag'];
-    selfLink = json['selfLink'];
-    volumeInfo = json['volumeInfo'] != null ? VolumeInfo.fromJson(json['volumeInfo']) : null;
-    saleInfo = json['saleInfo'] != null ? SaleInfo.fromJson(json['saleInfo']) : null;
-    accessInfo = json['accessInfo'] != null ? AccessInfo.fromJson(json['accessInfo']) : null;
+  factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
+        kind: json['kind'] as String?,
+        id: json['id'] as String?,
+        etag: json['etag'] as String?,
+        selfLink: json['selfLink'] as String?,
+        volumeInfo:
+            VolumeInfo.fromJson(json['volumeInfo'] as Map<String, dynamic>),
+        saleInfo: json['saleInfo'] == null
+            ? null
+            : SaleInfo.fromJson(json['saleInfo'] as Map<String, dynamic>),
+        accessInfo: json['accessInfo'] == null
+            ? null
+            : AccessInfo.fromJson(json['accessInfo'] as Map<String, dynamic>),
+        searchInfo: json['searchInfo'] == null
+            ? null
+            : SearchInfo.fromJson(json['searchInfo'] as Map<String, dynamic>),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'kind': kind,
+        'id': id,
+        'etag': etag,
+        'selfLink': selfLink,
+        'volumeInfo': volumeInfo?.toJson(),
+        'saleInfo': saleInfo?.toJson(),
+        'accessInfo': accessInfo?.toJson(),
+        'searchInfo': searchInfo?.toJson(),
+      };
+
+  @override
+  List<Object?> get props {
+    return [
+      kind,
+      id,
+      etag,
+      selfLink,
+      volumeInfo,
+      saleInfo,
+      accessInfo,
+      searchInfo,
+    ];
   }
-  String? kind;
-  String? id;
-  String? etag;
-  String? selfLink;
-  VolumeInfo? volumeInfo;
-  SaleInfo? saleInfo;
-  AccessInfo? accessInfo;
-
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['kind'] = kind;
-    map['id'] = id;
-    map['etag'] = etag;
-    map['selfLink'] = selfLink;
-    if (volumeInfo != null) {
-      map['volumeInfo'] = volumeInfo?.toJson();
-    }
-    if (saleInfo != null) {
-      map['saleInfo'] = saleInfo?.toJson();
-    }
-    if (accessInfo != null) {
-      map['accessInfo'] = accessInfo?.toJson();
-    }
-    return map;
-  }
-
 }
