d1e1aa2db88bd3089c625f5de302f97be82dc361
ismailrabie <ismailrabie111@gmail.com>
2024-12-27 11:09:12 +0200

    put data in  FeaturedBooksListView with state

--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'custom_list_view_item.dart';
+import '../../../../../core/widgets/custom_error_widget.dart';
+import '../../../../../core/widgets/custom_loading_indicator.dart';
+import '../../../manger/featured_books_cubit/featured_book_cubit.dart';
+import 'custom_book_image.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-      height: MediaQuery.of(context).size.height *.3,
-      child: ListView.builder(itemCount: 3,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: FeaturedListViewItem(),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: ListView.builder(
+              itemCount: state.bookModel.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: CustomBookImage(image: state.bookModel[index].volumeInfo?.imageLinks
+                      ?.thumbnail ??
+                      '',),
+                  );
+                }),
           );
-        },),
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errorMessage: state.errorMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
