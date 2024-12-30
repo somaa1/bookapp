131fa6ba13cd84928e70558ba2f743ab289c373c
ismailrabie <ismailrabie111@gmail.com>
2024-12-30 12:03:13 +0200

    some edit in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:bookapp/core/utils/app_router.dart';
-import 'package:bookapp/features/splash/presentation/views/splash_view.dart';
+import 'package:bookapp/core/utils/service_locator.dart';
+import 'package:bookapp/features/home/data/repo/home_repo_impl.dart';
+import 'package:bookapp/features/home/manger/featured_books_cubit/featured_book_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'constants.dart';
+import 'features/home/manger/newest_book/newest_book_cubit.dart';
 
 void main() {
+  setup();
   runApp(const BookApp());
 }
 
@@ -14,12 +19,23 @@ class BookApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:  kPrimeColor,
-      textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)) ,
-
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>())..fetchFeaturedBook(),
+        ),
+        BlocProvider(
+          create: (context) => NewestBookCubit(getIt.get<HomeRepoImpl>())..newestBookBooks(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimeColor,
+            textTheme:
+                GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
+      ),
     );
   }
 }
